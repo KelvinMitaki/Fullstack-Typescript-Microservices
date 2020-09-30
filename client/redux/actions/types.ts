d53954bf0268aaaf102afbb8daa24c94219b1345
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-30 23:02:52 +0300

    added types enum

---
+++ client/redux/actions/types.ts
@@ -0,0 +1,19 @@
+export const GET_MESSAGE = "GET_MESSAGE";
+export const CURRENT_USER = "CURRENT_USER";
+export const REGISTER_START = "REGISTER_START";
+export const REGISTER_STOP = "REGISTER_STOP";
+export const REGISTER_ERROR = "REGISTER_ERROR";
+export const LOADING_START = "LOADING_START";
+export const LOADING_STOP = "LOADING_STOP";
+export const LOGIN_ERROR = "LOGIN_ERROR";
+
+export enum Types {
+  GetMessage,
+  CurrentUser,
+  RegisterStart,
+  RegisterStop,
+  RegisterError,
+  LoadingStart,
+  LoadingStop,
+  LoadingError
+}
