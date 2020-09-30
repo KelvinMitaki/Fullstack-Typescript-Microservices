12dd379316d5a173f3f175e42fdbd88e928daaac
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-30 23:25:57 +0300

    added auth reducer

--- client/redux/actions/types.ts
+++ client/redux/actions/types.ts
@@ -1,12 +1,3 @@
-export const GET_MESSAGE = "GET_MESSAGE";
-export const CURRENT_USER = "CURRENT_USER";
-export const REGISTER_START = "REGISTER_START";
-export const REGISTER_STOP = "REGISTER_STOP";
-export const REGISTER_ERROR = "REGISTER_ERROR";
-export const LOADING_START = "LOADING_START";
-export const LOADING_STOP = "LOADING_STOP";
-export const LOGIN_ERROR = "LOGIN_ERROR";
-
 export enum Types {
   GetMessage,
   CurrentUser,
@@ -15,5 +6,5 @@ export enum Types {
   RegisterError,
   LoadingStart,
   LoadingStop,
-  LoadingError
+  LoginError
 }
