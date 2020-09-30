ff5bff2d0f62451d129a340caa1c8a287f11d597
KelvinMitaki <mitakikelvin1@gmail.com>
2020-10-01 00:11:43 +0300

    added textarea

---
+++ client/components/TextArea.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Form } from "semantic-ui-react";
+
+interface TextAreaInterface {
+  rows: number;
+  placeholder: string;
+}
+
+const TextArea = ({ rows, placeholder }: TextAreaInterface) => {
+  return (
+    <Form.Field>
+      <textarea placeholder={placeholder} rows={rows} />
+    </Form.Field>
+  );
+};
+
+export default TextArea;
