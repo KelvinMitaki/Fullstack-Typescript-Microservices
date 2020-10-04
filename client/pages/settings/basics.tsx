84a1f7d8da0876b8abfabb087b6b9837dd55207b
KelvinMitaki <mitakikelvin1@gmail.com>
2020-10-04 21:53:21 +0300

    fixed populating settings basics

--- client/pages/settings/basics.tsx
+++ client/pages/settings/basics.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useContext, useState } from "react";
 import {
   Button,
   Segment,
@@ -17,68 +17,76 @@ import { User } from "../../interfaces/User";
 import { BasicProfileFormValues } from "../../interfaces/Basics";
 import Axios from "axios";
 import withAuth from "../../hocs/withAuth";
+import { UserContext } from "../../contexts/userContext";
+import { connect } from "react-redux";
 
 interface Props {
   user: User | null;
 }
+interface State {
+  loading: boolean;
+  error: string | null;
+}
 
-export class basics extends Component<
-  InjectedFormProps<BasicProfileFormValues, Props> & Props
-> {
-  state = {
-    loading: false,
-    error: null
-  };
-  basicProfile = async (formValues: BasicProfileFormValues) => {
+let fetchedUser = {};
+const basics = (
+  props: InjectedFormProps<BasicProfileFormValues, Props> & Props
+) => {
+  const { user } = useContext(UserContext);
+  const [loading, SetLoading] = useState<State["loading"]>(false);
+  const [error, SetError] = useState<State["error"]>(null);
+  const basicProfile = async (formValues: BasicProfileFormValues) => {
     try {
       console.log(formValues);
-      this.setState({ loading: true });
+      SetLoading(true);
       await Axios.post("/api/user/profile/edit", formValues);
-      Router.push("/profile");
-      this.setState({ loading: false });
+      Router.push(`/profile/${user?._id}`);
+      SetLoading(false);
     } catch (error) {
       console.log(error);
-      this.setState({ error: "Error updating profile", loading: false });
+      SetLoading(false);
+      SetError("Error updating profile");
     }
   };
-  render() {
-    return (
-      <Layout title="Basics" user={this.props.user}>
-        <div className="profile">
-          <Grid stackable>
-            <Grid.Column width={12}>
-              <Segment>
-                <Header dividing size="large" content="Basics" />
-                <Form
-                  onSubmit={this.props.handleSubmit(
-                    (formValues: BasicProfileFormValues) =>
-                      this.basicProfile(formValues)
-                  )}
-                >
+  fetchedUser = user!;
+  return (
+    <Layout title="Basics" user={props.user}>
+      <div className="profile">
+        <Grid stackable>
+          <Grid.Column width={12}>
+            <Segment>
+              <Header dividing size="large" content="Basics" />
+              <Form
+                onSubmit={props.handleSubmit(
+                  (formValues: BasicProfileFormValues) =>
+                    basicProfile(formValues)
+                )}
+              >
+                <Field
+                  component={TextInput}
+                  type="text"
+                  name="knownAs"
+                  placeholder="Known As"
+                />
+
+                <Form.Group inline>
+                  <label>Gender: </label>
                   <Field
-                    component={TextInput}
-                    type="text"
-                    name="knownAs"
-                    placeholder="Known As"
+                    component={RadioButton}
+                    label="Male"
+                    value="male"
+                    name="gender"
+                    type="radio"
                   />
-                  <Form.Group inline>
-                    <label>Gender: </label>
-                    <Field
-                      component={RadioButton}
-                      label="Male"
-                      value="male"
-                      name="gender"
-                      type="radio"
-                    />
-                    <Field
-                      component={RadioButton}
-                      label="Female"
-                      value="female"
-                      name="gender"
-                      type="radio"
-                    />
-                  </Form.Group>
-                  {/* <Field
+                  <Field
+                    component={RadioButton}
+                    label="Female"
+                    value="female"
+                    name="gender"
+                    type="radio"
+                  />
+                </Form.Group>
+                {/* <Field
             width={8}
             name="dateOfBirth"
             component={DateInput}
@@ -88,48 +96,54 @@ export class basics extends Component<
             dropdownMode="select"
             maxDate={addYears(new Date(), -18)}
           /> */}
-                  <Field
-                    component={TextInput}
-                    name="homeTown"
-                    placeholder="Home Town"
-                    type="text"
-                  />
-                  <Divider />
-                  <Button
-                    disabled={this.props.pristine || this.state.loading}
-                    size="large"
-                    positive
-                    content="Update Profile"
-                    loading={this.state.loading}
-                  />
-                  <h5 style={{ color: "red" }}>{this.state.error}</h5>
-                </Form>
-              </Segment>
-            </Grid.Column>
-            <Grid.Column width={4}>
-              <SettingsNav />
-            </Grid.Column>
-          </Grid>
-        </div>
-        <style jsx>{`
-          .profile {
-            margin-top: 35vh;
-          }
-          @media screen and (min-width: 760px) {
-            .profile {
-              margin-top: 25vh;
-            }
-          }
-        `}</style>
-      </Layout>
-    );
-  }
-}
+                <Field
+                  component={TextInput}
+                  name="homeTown"
+                  placeholder="Home Town"
+                  type="text"
+                />
+                <Divider />
+                <Button
+                  disabled={props.pristine || loading}
+                  size="large"
+                  positive
+                  content="Update Profile"
+                  loading={loading}
+                />
+                <h5 style={{ color: "red" }}>{error}</h5>
+              </Form>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <SettingsNav />
+          </Grid.Column>
+        </Grid>
+      </div>
 
+      <style jsx>{`
+        .profile {
+          margin-top: 35vh;
+        }
+        @media screen and (min-width: 760px) {
+          .profile {
+            margin-top: 25vh;
+          }
+        }
+      `}</style>
+    </Layout>
+  );
+};
+const mapStateToProps = () => {
+  return {
+    initialValues: fetchedUser
+  };
+};
 export default withAuth(
-  reduxForm<BasicProfileFormValues, Props>({
-    form: "basics",
-    enableReinitialize: true,
-    destroyOnUnmount: false
-  })(basics)
+  connect(mapStateToProps)(
+    reduxForm<BasicProfileFormValues, Props>({
+      form: "basics",
+      enableReinitialize: true,
+      destroyOnUnmount: false
+    })(basics)
+  )
 );
