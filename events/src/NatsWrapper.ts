93f09ca17649f4b6c39fef0bc25ddc7f4749c4dc
KelvinMitaki <mitakikelvin1@gmail.com>
2020-10-07 11:22:58 +0300

    added nats to events microservice

---
+++ events/src/NatsWrapper.ts
@@ -0,0 +1,26 @@
+import { Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
+
+export class NatsWrapper {
+  private _client?: Stan;
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot access nats before connecting");
+    }
+    return this._client;
+  }
+  connect(ClusterId: string, clientId: string, url: string): Promise<void> {
+    this._client = nats.connect(ClusterId, clientId, { url });
+    return new Promise((resolve, reject) => {
+      this.client.on("connect", () => {
+        console.log("connected to NATS");
+        resolve();
+      });
+      this.client.on("error", err => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
--- events/src/index.ts
+++ events/src/index.ts
@@ -1,11 +1,29 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./NatsWrapper";
 
 const start = async (): Promise<void> => {
   try {
-    if (!process.env.COOKIE_SECRET || !process.env.MONGO_URI) {
+    if (
+      !process.env.COOKIE_SECRET ||
+      !process.env.MONGO_URI ||
+      !process.env.NATS_CLUSTER_ID ||
+      !process.env.NATS_CLIENT_ID ||
+      !process.env.NATS_URL
+    ) {
       throw new Error("Env variables must be provided");
     }
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("Nats Connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
